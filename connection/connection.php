cfdaf42007a350788ebb6942e92d867e3115b145
Sid <d.legend.1993@gmail.com>
2015-11-05 23:25:12 +0530

    updating

--- connection/connection.php
+++ connection/connection.php
@@ -1,7 +1,16 @@
 <?php
-	
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+	echo $server;
+	echo $username;
+	echo $password;
+	echo $db;
 
 
 	$connection=mysql_connect('us-cdbr-iron-east-03.cleardb.net:3306','b3934deb5eca6f','75eb4c64');
+	echo $connection;
 	$db=mysql_select_db($db);
 ?> 
