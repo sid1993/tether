02188b155a05aec65c83f78dffd5a4462c023161
Sid <d.legend.1993@gmail.com>
2015-11-05 23:27:00 +0530

    updating

--- connection/connection.php
+++ connection/connection.php
@@ -10,7 +10,6 @@
 	echo $db;
 
 
-	$connection=mysql_connect('us-cdbr-iron-east-03.cleardb.net:3306','b3934deb5eca6f','75eb4c64');
-	echo $connection;
+	echo mysql_connect('us-cdbr-iron-east-03.cleardb.net:3306','b3934deb5eca6f','75eb4c64');
 	$db=mysql_select_db($db);
 ?> 
