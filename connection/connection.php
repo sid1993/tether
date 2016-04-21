5d3a2741d43be615e0b35fc2c3f5c0ec571df14f
Sid <d.legend.1993@gmail.com>
2016-04-22 01:21:12 +0530

    Update

--- connection/connection.php
+++ connection/connection.php
@@ -1,4 +1,4 @@
 <?php
-	$connection=mysql_connect('us-cdbr-iron-east-03.cleardb.net','b3934deb5eca6f','Ss@1998042');
+	$connection=mysql_connect('us-cdbr-iron-east-03.cleardb.net','b3934deb5eca6f','75eb4c64');
 	$db=mysql_select_db('heroku_78c30c5595ce4d9');
 ?> 
