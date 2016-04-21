5acf8fb126dc12a99089a9008023b9ffddb0cbf0
Sid <d.legend.1993@gmail.com>
2016-04-22 01:25:22 +0530

    Update

--- connection/connection.php
+++ connection/connection.php
@@ -1,4 +1,10 @@
 <?php
-	$connection=mysql_connect('us-cdbr-iron-east-03.cleardb.net','b3934deb5eca6f','75eb4c64');
-	$db=mysql_select_db('heroku_78c30c5595ce4d9');
-?> 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+?>
