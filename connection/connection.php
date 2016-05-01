90a5dae34b4e183e2484e887d16130a7ed6006a4
Sid <d.legend.1993@gmail.com>
2016-05-01 12:32:53 +0530

    update

--- connection/connection.php
+++ connection/connection.php
@@ -1,10 +1,11 @@
 <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = "p:"+$url["host"];
+$server =$url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
+$hostname="p:"+$server;
 
 $conn = new mysqli($server, $username, $password, $db);
 ?>
