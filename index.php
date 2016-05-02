7910f8c3506ef1ba45fc4a688d0f493965a9e084
Sid <d.legend.1993@gmail.com>
2016-05-02 18:53:48 +0530

    updateajax

--- index.php
+++ index.php
@@ -190,7 +190,7 @@
       embedParams: {
         generated: <?php print $generated; ?>,
         signature: "<?php print $signature; ?>",
-		code:"<?php echo $selected_uid; ?>"
+		code:"<?php echo $selected_uid[0]; ?>"
       }
     });
 	embed
