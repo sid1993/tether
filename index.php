dcb2d1f76a69d40e0c36cdbe9b0655fa0dd10665
Sid <d.legend.1993@gmail.com>
2016-05-02 21:35:36 +0530

    updateajax

--- index.php
+++ index.php
@@ -228,7 +228,7 @@
                               url:"queque.php",
                               cache: false,
           					success: function(data){
-             					$("#gruveo").load("index.php#gruveo");
+             					$("#gruveo").html("#gruveo");
           						} 
  
                           });
