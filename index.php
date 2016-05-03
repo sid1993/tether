f623c189105c4b3208573221377a57267266040e
Sid <d.legend.1993@gmail.com>
2016-05-03 23:38:39 +0530

    update

--- index.php
+++ index.php
@@ -195,7 +195,7 @@
 	  if(!generated_code)
 	  {
 		  document.getElementById("myembed").innerHTML="<h1>Please Wait...All Our Doctors Are Busy...<h1>";
-		  setTimeout(location.reload(), 60000);
+		  setTimeout(queque, 3000);
 	  }
 	  else
 	  {
@@ -221,6 +221,16 @@
 		{
 			alert('Calling Dr.Sharma');
 		}
+	  }
+	  function queque()
+	  {
+		  $.ajax({
+                              url:"queque.php",
+                              success:function(data){
+                                 
+                              }
+ 
+                          });
 	  }
 
 </script>
--- queque.php
+++ queque.php
@@ -2,6 +2,7 @@
 session_start();
 include "connection/connection.php";
 
+
 $qsql="SELECT uid FROM heroku_78c30c5595ce4d9.registration where status_code=1;";
 	$qres=mysqli_query($conn,$qsql);
 	$idle_user = array();
@@ -10,10 +11,6 @@ $qsql="SELECT uid FROM heroku_78c30c5595ce4d9.registration where status_code=1;"
 	}
 	if($idle_user)
 	{
-		//random uid selection
-		$length=sizeof($idle_user);
-		$randomIndex=rand(0,$length-1);
-		$_SESSION['$selected_uid']=$idle_user[$randomIndex]['uid'];
-		echo "<script>onGruveoEmbedAPIReady();</script>";
+		echo "<script>loaction.reload();</script>";
 	}
 ?>
