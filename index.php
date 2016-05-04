bf9fe5bcaa21e1bfb3d30e684ff6c08e682fb3a3
Sid <d.legend.1993@gmail.com>
2016-05-04 15:14:21 +0530

    upDate

--- index.php
+++ index.php
@@ -194,7 +194,7 @@
 	var generated_code="<?php echo $_SESSION['$selected_uid']; ?>";
 	  if(!generated_code)
 	  {
-		  document.getElementById("myembed").innerHTML="<h1 align='center'>Please Wait...All Our Doctors Are Busy...<h1>";
+		  document.getElementById("myembed").innerHTML="<blink><h1 align='center'>Please Wait...All Our Doctors Are Busy...<h1></blink>";
 		  queque();
 	  }
 	  else
