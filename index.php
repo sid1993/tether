9ffbbdcdae422bd9f9335703964ee3d90147afeb
Sid <d.legend.1993@gmail.com>
2016-05-09 19:41:00 +0530

    update

--- index.php
+++ index.php
@@ -228,8 +228,14 @@
 	  }
 	  function onBusy()
 	  {
-		  document.getElementById("myembed").innerHTML="<h1 align='center'>Please Wait...While We Find Another Doctor For You...<img src='img/load.gif' width='300' height='300'/><h1>";
-		  queque();
+		  $.ajax({
+                              url:"busy.php",
+                             success: function(data) {
+								("#myembed").load(data);
+								queque();
+							 }
+                          });
+		  
 	  }
 	  function queque()
 	  {
